b5506273b78219bdcafbab5da57fd1a81442f426
eric.oldre <eric.oldre@a0bf3537-8e53-448f-ae4a-0b965e85af5e>
2012-04-26 02:24:36 +0000

    git-svn-id: https://subversion.assembla.com/svn/sinobyl/trunk@61 a0bf3537-8e53-448f-ae4a-0b965e85af5e

---
+++ Sinobyl/Sinobyl.WPF/ViewModels/BoardSquareVM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sinobyl.Engine;
+namespace Sinobyl.WPF.ViewModels
+{
+    public class BoardSquareVM: ViewModelBase 
+    {
+        private readonly Sinobyl.Engine.ChessPosition _position;
+
+        public string Name
+        {
+            get { return _position.PositionToString(); }
+            
+        }
+        public bool IsLight
+        {
+            get { return (int)_position%2 == 0; }
+        }
+
+        public BoardSquareVM(Sinobyl.Engine.ChessPosition pos)
+        {
+            _position = pos;
+        }
+
+        public override string  ToString()
+        {
+ 	        return Name;
+        }
+
+        public static List<BoardSquareVM> AllBoardSquares()
+        {
+            List<BoardSquareVM> retval = new List<BoardSquareVM>();
+            foreach (var pos in Sinobyl.Engine.Chess.AllPositions)
+            {
+                retval.Add(new BoardSquareVM(pos));
+            }
+            return retval;
+        }
+    }
+}
---
+++ Sinobyl/Sinobyl.WPF/ViewModels/BoardVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Sinobyl.Engine;
+namespace Sinobyl.WPF.ViewModels
+{
+    public class BoardVM: ViewModelBase
+    {
+        public ObservableCollection<BoardSquareVM> Squares { get; private set; }
+
+        public BoardVM()
+        {
+            Squares = new ObservableCollection<BoardSquareVM>(BoardSquareVM.AllBoardSquares());
+        }
+
+        public static BoardVM GetDesignBoardVM()
+        {
+            return new BoardVM();
+        }
+    }
+}

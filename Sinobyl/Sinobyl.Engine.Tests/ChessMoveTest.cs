bb883b4bb391142e6a8908881de5920272bf8070
eric.oldre <eric.oldre@a0bf3537-8e53-448f-ae4a-0b965e85af5e>
2014-03-15 18:58:33 +0000

    git-svn-id: https://subversion.assembla.com/svn/sinobyl/trunk@177 a0bf3537-8e53-448f-ae4a-0b965e85af5e

--- Sinobyl/Sinobyl.Engine.Tests/ChessMoveTest.cs
+++ Sinobyl/Sinobyl.Engine.Tests/ChessMoveTest.cs
@@ -14,7 +14,51 @@ namespace Sinobyl.Engine.Tests
     [TestClass()]
     public class ChessMoveTest
     {
+        [TestMethod]
+        public void ValidateFromToProm()
+        {
+            foreach (ChessPosition from in ChessPositionInfo.AllPositions)
+            {
+                foreach (ChessPosition to in ChessPositionInfo.AllPositions)
+                {
+                    foreach (ChessPiece prom in ChessPieceInfo.AllPieces.Concat(new ChessPiece[] {ChessPiece.EMPTY}))
+                    {
+                        ChessMove move = ChessMoveInfo.Create(from, to, prom);
 
+                        var from2 = move.From();
+                        var to2 = move.To();
+                        var prom2 = move.Promote();
+
+                        Assert.AreEqual<ChessPosition>(from, from2);
+                        Assert.AreEqual<ChessPosition>(to, to2);
+                        Assert.AreEqual<ChessPiece>(prom, prom2);
+                        Assert.AreNotEqual<ChessMove>(move, ChessMove.NULL_MOVE);
+                    }
+                }    
+            }
+        }
+
+        [TestMethod]
+        public void ValidateFromTo()
+        {
+            foreach (ChessPosition from in ChessPositionInfo.AllPositions)
+            {
+                foreach (ChessPosition to in ChessPositionInfo.AllPositions)
+                {
+
+                    ChessMove move = ChessMoveInfo.Create(from, to, ChessPiece.EMPTY);
+
+                    var from2 = move.From();
+                    var to2 = move.To();
+                    var prom2 = move.Promote();
+
+                    Assert.AreEqual<ChessPosition>(from, from2);
+                    Assert.AreEqual<ChessPosition>(to, to2);
+                    Assert.AreEqual<ChessPiece>(ChessPiece.EMPTY, prom2);
+                    Assert.AreNotEqual<ChessMove>(move, ChessMove.NULL_MOVE);
+                }
+            }
+        }
 
 
 

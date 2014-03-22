52fbf01c6dfb5b6da7888b1ae575b229e27a12af
eric.oldre <eric.oldre@a0bf3537-8e53-448f-ae4a-0b965e85af5e>
2014-03-22 20:26:41 +0000

    git-svn-id: https://subversion.assembla.com/svn/sinobyl/trunk@200 a0bf3537-8e53-448f-ae4a-0b965e85af5e

--- Sinobyl/Sinobyl.Windows/ViewModel/WelcomeVM.cs
+++ Sinobyl/Sinobyl.Windows/ViewModel/WelcomeVM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Sinobyl.Windows.ViewModel
 {
@@ -12,9 +13,46 @@ namespace Sinobyl.Windows.ViewModel
         public WelcomeVM(Common.IMessenger messenger)
             : base(messenger)
         {
+            GalaSoft.MvvmLight.Messaging.MessageBase
+        }
+
+
+        
+        public ICommand PlayAsWhiteCommand
+        {
+            get
+            {
+                return new GalaSoft.MvvmLight.Command.RelayCommand(PlayAsWhite);
+            }
+        }
+        public void PlayAsWhite()
+        {
 
         }
 
+        public ICommand PlayAsBlackCommand
+        {
+            get
+            {
+                return new GalaSoft.MvvmLight.Command.RelayCommand(PlayAsBlack);
+            }
+        }
+        public void PlayAsBlack()
+        {
+
+        }
+
+        public ICommand ComputerVsComputerCommand
+        {
+            get
+            {
+                return new GalaSoft.MvvmLight.Command.RelayCommand(ComputerVsComputer);
+            }
+        }
+        public void ComputerVsComputer()
+        {
+
+        }
 
 
     }

c1cbb6cf5bcc7a16019e9400d1e72a5f281eb574
eric.oldre <eric.oldre@a0bf3537-8e53-448f-ae4a-0b965e85af5e>
2014-03-22 18:34:14 +0000

    git-svn-id: https://subversion.assembla.com/svn/sinobyl/trunk@196 a0bf3537-8e53-448f-ae4a-0b965e85af5e

---
+++ Sinobyl/Sinobyl.Windows/Common/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinobyl.Windows.Common
+{
+    public abstract class ViewModelBase: GalaSoft.MvvmLight.ViewModelBase
+    {
+        protected readonly Common.IMessenger _messenger;
+
+        public ViewModelBase(Common.IMessenger messenger)
+        {
+            _messenger = messenger;
+        }
+
+        public Common.IMessenger Messenger
+        {
+            get { return _messenger; }
+        }
+    }
+}

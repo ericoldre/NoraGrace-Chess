b27aafcaaf21ad18e0a90662803f8c11add87833
eric.oldre <eric.oldre@a0bf3537-8e53-448f-ae4a-0b965e85af5e>
2014-04-05 20:38:54 +0000

    git-svn-id: https://subversion.assembla.com/svn/sinobyl/trunk@220 a0bf3537-8e53-448f-ae4a-0b965e85af5e

--- Sinobyl/Sinobyl.Engine.Tests/SearchTest.cs
+++ Sinobyl/Sinobyl.Engine.Tests/SearchTest.cs
@@ -53,11 +53,39 @@ namespace Sinobyl.Engine.Tests
 
         }
 
-
-        void search_ProgressReported(object sender, SearchProgressEventArgs e)
+        [TestMethod]
+        public void StaticExchangeTest()
         {
-            throw new NotImplementedException();
+            ChessFEN fen = new ChessFEN("rnbqkb1r/ppp2ppp/4p3/3p1n2/4P1P1/1B4N1/PPPP1P1P/RNBQK2R w KQkq - 0 1 ");
+            ChessBoard board = new ChessBoard(fen);
+
+            AssertFirstGreaterSEE(board, "e4f5", "g3f5");
+            AssertFirstGreaterSEE(board, "g4f5", "g3f5");
+            AssertFirstGreaterSEE(board, "e4f5", "g3f5");
+            AssertFirstGreaterSEE(board, "g3f5", "b3d5");
+
+            //fen = new ChessFEN("r2qr1k1/pp3pp1/1bp1b2p/8/3P1P2/Q1PB1N2/P4PPP/1R3K1R w - - 3 19 ");
+            //board = new ChessBoard(fen);  //"Qxd7 Qxf7+ Qxc6 Qxa5"
+            ////AssertFirstGreaterSEE(board, "xxxx", "xxxx");
+            ////AssertFirstGreaterSEE(board, "xxxx", "xxxx");
+            ////AssertFirstGreaterSEE(board, "xxxx", "xxxx");
+            ////AssertFirstGreaterSEE(board, "xxxx", "xxxx");
+            ////AssertFirstGreaterSEE(board, "xxxx", "xxxx");
+
+
+            //string caps = string.Join(" ", ChessMove.GenMoves(board, true).Select(m => m.ToString(board)).ToArray());
+
+            //int i = caps.Length;
         }
-        //
+
+        private void AssertFirstGreaterSEE(ChessBoard board, string move1, string move2)
+        {
+            ChessMove m1 = new ChessMove(board, move1);
+            ChessMove m2 = new ChessMove(board, move2);
+            int score1 = ChessMove.Comp.CompEstScoreSEE(m1, board);
+            int score2 = ChessMove.Comp.CompEstScoreSEE(m2, board);
+            Assert.IsTrue(score1 > score2);
+        }
     }
 }
+

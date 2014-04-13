ac3a18c78a46f377a9c3a300cde803e0e8e8ce75
eric.oldre <eric.oldre@a0bf3537-8e53-448f-ae4a-0b965e85af5e>
2014-04-13 20:28:12 +0000

    git-svn-id: https://subversion.assembla.com/svn/sinobyl/trunk@262 a0bf3537-8e53-448f-ae4a-0b965e85af5e

--- Sinobyl/Sinobyl.Engine/ChessBitboard.cs
+++ Sinobyl/Sinobyl.Engine/ChessBitboard.cs
@@ -63,17 +63,7 @@ namespace Sinobyl.Engine
             //    + _byteBitcount[(((ulong)bitboard >> 56) & 255)];
         }
 
-        private static int[] debrujinPositions =
-	    {
-	        0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
-	        31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
-	    };
-
-        private static int debrujinLSB(int number)
-        {
-            uint res = unchecked((uint)(number & -number) * 0x077CB531U) >> 27;
-            return debrujinPositions[res];
-        }
+        
 
         public static ChessPosition NorthMostPosition(this ChessBitboard bitboard)
         {
@@ -221,6 +211,17 @@ namespace Sinobyl.Engine
             }
         }
 
+        private static int[] debrujinPositions =
+	    {
+	        0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+	        31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+	    };
+
+        private static int debrujinLSB(int number)
+        {
+            return debrujinPositions[unchecked((uint)(number & -number) * 0x077CB531U) >> 27];
+        }
+
         public static ChessPosition PopFirst(ref ChessBitboard bitboard)
         {
             System.Diagnostics.Debug.Assert(bitboard != ChessBitboard.Empty);
@@ -229,13 +230,13 @@ namespace Sinobyl.Engine
             ChessPosition first;
             if (((ulong)bitboard & 0xFFFFFFFF) != 0)
             {
-                ulong x = (ulong)bitboard & 0xFFFFFFFF;
-                first = (ChessPosition)debrujinLSB((int)x);
+                int number = (int)((ulong)bitboard & 0xFFFFFFFF);
+                first = (ChessPosition)debrujinPositions[unchecked((uint)(number & -number) * 0x077CB531U) >> 27];
             }
             else
             {
-                ulong x = (ulong)bitboard >> 32;
-                first = (ChessPosition)(debrujinLSB((int)x) + 32);
+                int number = (int)((ulong)bitboard >> 32);
+                first = (ChessPosition)(debrujinPositions[unchecked((uint)(number & -number) * 0x077CB531U) >> 27] + 32);
             }
 
             bitboard = bitboard & ~first.Bitboard();

709cce1ab926ee8a14e33a5dd731c53d52f9fcb7
Eric Oldre <eric.oldre@gmail.com>
2014-06-29 12:46:53 -0500

    added array init helpers.

---
+++ NoraGrace/NoraGrace.Engine/Helpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoraGrace.Engine
+{
+    public static class Helpers
+    {
+        private static T[][] ArrayInit<T>(int x, int y)
+        {
+            T[][] retval = new T[x][];
+            for (int i = 0; i < retval.Length; i++)
+            {
+                retval[i] = new T[y];
+            }
+            return retval;
+        }
+
+        private static T[][][] ArrayInit<T>(int x, int y, int z)
+        {
+            T[][][] retval = new T[x][][];
+            for (int xi = 0; xi < retval.Length; xi++)
+            {
+                retval[xi] = new T[y][];
+                for (int yi = 0; yi < retval[xi][yi].Length; yi++)
+                {
+                    retval[xi][yi] = new T[z];
+                }
+            }
+            return retval;
+        }
+
+    }
+}
